f7271895a13dfb5960eb4eb4d2d4158c631fb830
sigmaclientwastaken <blockmbot.2@gmail.com>
2021-09-10 18:16:34 +0200

    Edited default settings
    - Changed ClickGUI to have gradients off by default.
    - Changed ArrayList to have gradients off by default.

--- minecraft/me/wavelength/baseclient/module/modules/client/ClickGui.java
+++ minecraft/me/wavelength/baseclient/module/modules/client/ClickGui.java
@@ -19,7 +19,7 @@ public class ClickGui extends Module {
 		moduleSettings.addDefault("offset", 1);
 		moduleSettings.addDefault("speed", 20);
 		moduleSettings.addDefault("spacing", 5);
-		moduleSettings.addDefault("gradient", true);
+		moduleSettings.addDefault("gradient", false);
 		moduleSettings.addDefault("scroll speed", 10);
 	}
 
